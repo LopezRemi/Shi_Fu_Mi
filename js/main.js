e8e9202d1cdf59bb140e38f3f980d129a8beefb5
Ryuu76000 <r.lopez76000@gmail.com>
2021-09-17 08:56:44 +0200

    ajout de la fonction Ai

--- js/main.js
+++ js/main.js
@@ -1,37 +1,48 @@
- alert("Bienvenue sur le jeu 'Shi fu Mi'");
+ alert("Bienvenue sur le jeu 'Shi fu Mi' fabriqué par mes soins");
 
-function greetings(firstname) {
-    return "Bienvenue" + firstname;
-}
+//variable utisilé dans le choix du Prénom
+var g = false
+var n = false
 
-function noNumber(firstname) {
-    for (var i = 0 ; i < firstname.length; i++) {
-        alert(firstname[i])
-    }
-}
-
-
-var v = false
-
-    while (v == false) {
-        firstname = prompt("qu'elle est votre prénom ?");
-        noNumber(firstname);
-
-    if (firstname.length < 15 && firstname.length != 0) {
+//boucle utilisé pour le choix du prénom
+    while (n == false) {
+        firstname = prompt("Qu'elle est votre prénom ?");
+        var n = false
+    if (firstname.length >= 3 && firstname.length != null) {
         alert("Merci "+ firstname)
-        v = true
+       var n = true
     }
-    else {
-        alert("Ce prénom est trop long");
-    }}  
+    if (firstname.length < 3 && firstname.length > 0) {
+        alert("Prénom trop court")
+        var n = false
+    }
+    if (firstname.length == 0) {
+        alert("Veuillez entrer un prénom")
+        var n = false
+ }
+}  
 
 
-
+//fonction utilisé pour déterminer le choix de l'ordinateur.
+function moveOfAi () {
  
+    var compMove = Math.random();
+if (compMove < 0.34) {
+    compMove = "pierre";
+} else if(compMove <= 0.67) {
+    compMove = "feuille";
+} else {
+    compMove = "ciseaux";
+    }
+  return
+}
+
+//fonction utilisé pour déterminer le choix du joueur.
+function moveOfPlayer () {
+    var playerMove = 
+}
 
 
 
 
 
-
-    
